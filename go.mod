324782dd299164c0ad81b96eb42da60a3bc79b9c
hirosuzuki <hiro.suzuki.oct10@gmail.com>
2022-01-09 05:41:19 +0000

    Rawモード

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hirosuzuki/go8080
 
 go 1.17
+
+require golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e
--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/hirosuzuki/go8080/i8080"
+	"golang.org/x/sys/unix"
 )
 
 type Memory struct {
@@ -13,6 +15,7 @@ type Memory struct {
 }
 
 func (m *Memory) Read(addr uint16) byte {
+	// https://www.autometer.de/unix4fun/z80pack/
 	return m.buf[addr]
 }
 
@@ -32,6 +35,31 @@ func (m *IOPort) Write(addr uint16, value byte) {
 	m.buf[addr] = value
 }
 
+func setRawMode() func() {
+	// 参考: https://github.com/mattn/go-tty/blob/master/tty_unix.go#L92
+	var ioctlReadTermios uint = 0x5401  // syscall.TCGETS
+	var ioctlWriteTermios uint = 0x5402 // syscall.TCSETS
+
+	termios, err := unix.IoctlGetTermios(0, ioctlReadTermios)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	backup := *termios
+
+	termios.Lflag &^= unix.ECHO | unix.ICANON | unix.ISIG
+
+	if err := unix.IoctlSetTermios(0, ioctlWriteTermios, termios); err != nil {
+		log.Fatal(err)
+	}
+
+	return func() {
+		if err = unix.IoctlSetTermios(0, ioctlWriteTermios, &backup); err != nil {
+			log.Fatal("Restore Termios Fail:", err)
+		}
+	}
+}
+
 func main() {
 	memory := &Memory{}
 	cpu := i8080.CPU{Memory: memory, IOPort: &IOPort{}}
@@ -46,4 +74,15 @@ func main() {
 	cpu.Exec(100, func(p *i8080.CPU) {
 		fmt.Println(p.Status())
 	})
+
+	defer setRawMode()()
+
+	for {
+		buf := make([]byte, 1)
+		n, err := os.Stdin.Read(buf)
+		fmt.Println(buf[0], n, err)
+		if buf[0] == 'e' {
+			break
+		}
+	}
 }
